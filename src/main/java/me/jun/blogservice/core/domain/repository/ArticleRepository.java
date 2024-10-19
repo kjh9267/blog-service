d59f34bc03d8c031e79b0c08c4c4a0554b59a75a
kjh9267 <intellijunho@kakao.com>
2024-10-19 21:06:18 +0900

    feat: Create deleteAllByWriter in articleRepository -test: Create deleteAllByWriterTest in articleRepository test

--- src/main/java/me/jun/blogservice/core/domain/repository/ArticleRepository.java
+++ src/main/java/me/jun/blogservice/core/domain/repository/ArticleRepository.java
@@ -1,12 +1,19 @@
 package me.jun.blogservice.core.domain.repository;
 
 import me.jun.blogservice.core.domain.Article;
+import me.jun.blogservice.core.domain.Writer;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     List<Article> findAllBy(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Article a where a.writer = :writer")
+    void deleteAllByWriter(Writer writer);
 }
